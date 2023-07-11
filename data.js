3ebe0ef2deb9f5e94af8dec8961951cccb1129e9
Muskan Sethiya <79002391+Muskan-Sethiya@users.noreply.github.com>
2023-07-11 02:28:48 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs=[
+    {
+        name:'Tin Whistle Background Music',
+        path:'musics/song1.mp3',
+        artist:'juliusH',
+        cover:'images/img1.jfif'
+    },
+{
+    name:'Concert Grand Piano',
+    path:'musics/song2.mp3',
+    artist:'juliusH',
+    cover:'images/img2.jfif'
+},
+{
+    name:'thebirdbrand',
+    path:'musics/song3.mp3',
+    artist:'Ali McKenzie',
+    cover:'images/img3.jpg'
+},
+{
+    name:'Onoychenkomusic',
+    path:'musics/song4.mp3',
+    artist:'Yevhen Onoychenko',
+    cover:'images/img4.jfif'
+}
+]
